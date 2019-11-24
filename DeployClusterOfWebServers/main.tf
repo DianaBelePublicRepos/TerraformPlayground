25e453133e51bee583bf0f5c178f502253ae7818
DianaBelePublicRepos <dianabeleemea@gmail.com>
2019-11-24 00:16:36 +0000

    Added autoscaling group tf configuration

---
+++ DeployClusterOfWebServers/main.tf
@@ -0,0 +1,66 @@
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default     = 8080
+}
+
+data "aws_availability_zones" "all" {}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-40d28157"
+  instance_type   = "t2.micro"
+  security_groups = ["${aws_security_group.instance.id}"]
+
+  user_data = <<-EOF
+                           #!/bin/bash
+                           echo "Hello World" > index.html
+                           nohup busybox httpd -f -p "${var.server_port}" &
+                           EOF
+
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-sg-name-third_SG"
+  tags = {
+    Name        = "Custom SG for Web Server"
+    Environment = "production"
+  }
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.id
+  availability_zones   = data.aws_availability_zones.all.names
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+output launch_configuration_id {
+  value = "${aws_launch_configuration.example.id}"
+}
